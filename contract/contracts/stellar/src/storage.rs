fd5e480feb21d891d23b289f30c03d6549fab06b
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-01-27 11:01:17 +0100

    feat: Afri scafold

--- contract/contracts/stellar/src/storage.rs
+++ contract/contracts/stellar/src/storage.rs
@@ -1,29 +1,29 @@
-use soroban_sdk::{Env, Address, Map};
+use soroban_sdk::{Env, Address, Map, Symbol, symbol_short};
 
-const ADMIN_KEY: &str = "ADMIN";
-const BALANCES_KEY: &str = "BALANCES";
+const ADMIN_KEY: Symbol = symbol_short!("ADMIN");
+const BALANCES_KEY: Symbol = symbol_short!("BAL");
 
 pub fn set_admin(env: &Env, admin: &Address) {
-    env.storage().instance().set(ADMIN_KEY, admin);
+    env.storage().instance().set(&ADMIN_KEY, admin);
 }
 
 pub fn get_admin(env: &Env) -> Address {
-    env.storage().instance().get(ADMIN_KEY).unwrap()
+    env.storage().instance().get(&ADMIN_KEY).unwrap()
 }
 
 fn balances(env: &Env) -> Map<Address, i128> {
     env.storage()
         .instance()
-        .get(BALANCES_KEY)
+        .get(&BALANCES_KEY)
         .unwrap_or(Map::new(env))
 }
 
 pub fn get_balance(env: &Env, user: &Address) -> i128 {
-    balances(env).get(user).unwrap_or(0)
+    balances(env).get(user.clone()).unwrap_or(0)
 }
 
 pub fn set_balance(env: &Env, user: &Address, amount: i128) {
     let mut map = balances(env);
     map.set(user.clone(), amount);
-    env.storage().instance().set(BALANCES_KEY, &map);
+    env.storage().instance().set(&BALANCES_KEY, &map);
 }
